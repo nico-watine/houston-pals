508c09ab61aef25c4ebe3a608ea844a15f885e01
Nico Watine <nico@studioncreations.com>
2016-09-09 14:49:45 -0500

    Created products page

---
+++ src/pages/products.php
@@ -0,0 +1,17 @@
+---
+url: /products
+title: Products
+description:
+og-title: Products
+og-description:
+og-image:
+body-id: 
+body-class: coming-soon
+---
+<div class="row" id="top-row">
+	<div class="medium-push-1 medium-11 columns">
+		<h1 class="title">Blog</h1>
+		<h2>Coming Soon</h2>
+	</div>
+</div>
+
