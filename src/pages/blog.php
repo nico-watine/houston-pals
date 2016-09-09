112c03610c83d76ae442d4d78bc8437b72d9e440
Nico Watine <nico@studioncreations.com>
2016-09-09 14:46:15 -0500

    Basic "coming soon" inserted

---
+++ src/pages/blog.php
@@ -0,0 +1,17 @@
+---
+url: /blog
+title: Blog
+description:
+og-title: Blog
+og-description:
+og-image:
+body-id: 
+body-class: coming-soon
+---
+<div class="row" id="top-row">
+	<div class="medium-push-1 medium-11 columns">
+		<h1 class="title">Blog</h1>
+		<h2>Coming Soon</h2>
+	</div>
+</div>
+
