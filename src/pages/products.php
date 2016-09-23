7b8e83fd460cff7ab1bb0090ff2d3431b7b95ce0
Nico Watine <nico@studioncreations.com>
2016-09-23 15:12:33 -0500

    New products layout altered
    Now .content-wrapper > .content is within src/pages/products.html so that we can include a full width section.
    
    Will have to consider re-shaping layout default so that products doesn’t need its own custom layout (for easier future code maintenance)

--- src/pages/products.php
+++ src/pages/products.php
@@ -9,12 +9,16 @@ body-id: products
 body-class: 
 layout: products
 ---
-<div class="row page-intro">
-	<div class="medium-push-1 medium-11 columns">
-		<h1 class="title">Products</h1>
-		<div class="row">
+<div class="content-wrapper">
+	<div class="content">
+		<div class="row page-intro">
 			<div class="medium-push-1 medium-11 columns">
-				<h3>We specialize in two main types of products - Orthotics and Prosthetics.</h3>
+				<h1 class="title">Products</h1>
+				<div class="row">
+					<div class="medium-push-1 medium-11 columns">
+						<h3>We specialize in two main types of products - Orthotics and Prosthetics.</h3>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
