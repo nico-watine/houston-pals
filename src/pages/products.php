549c283602b4406fde03e224533ab0931ce859b7
Nico Watine <nico@studioncreations.com>
2016-09-23 14:32:02 -0500

    Cleared up products page
    This is the next development area

--- src/pages/products.php
+++ src/pages/products.php
@@ -5,13 +5,8 @@ description: Products for Pet Artificial Limbs & Supports
 og-title: Products - Coming Soon
 og-description: More info on products for Pet Artificial Limbs & Supports coming soon
 og-image: /img/assets/og/1/og-image-1.jpg
-body-id: 
-body-class: coming-soon
+body-id: products
+body-class: 
 ---
-<div class="row" id="top-row">
-	<div class="medium-push-1 medium-10 columns">
-		<h1 class="title">Products</h1>
-		<h2>Coming Soon</h2>
-	</div>
-</div>
+
 
