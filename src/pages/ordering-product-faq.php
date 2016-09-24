e249cf2be3ce75b118c2a62582cd8a575047e226
Nico Watine <nico@studioncreations.com>
2016-09-24 07:40:31 -0500

    Removed duplicate .content-wrapper
    Since this page uses default layout (no full width banner sections)

--- src/pages/ordering-product-faq.php
+++ src/pages/ordering-product-faq.php
@@ -8,16 +8,12 @@ og-image: /img/assets/og/1/og-image-1.jpg
 body-id: ordering-product-faq
 body-class: 
 ---
-<div class="content-wrapper">
-	<div class="content no-paw">
-		<div class="row page-intro">
+<div class="row page-intro">
+	<div class="medium-push-1 medium-11 columns">
+		<h1 class="title">Ordering + Product FAQ</h1>
+		<div class="row">
 			<div class="medium-push-1 medium-11 columns">
-				<h1 class="title">Ordering + Product FAQ</h1>
-				<div class="row">
-					<div class="medium-push-1 medium-11 columns">
-						<h3>We specialize in two main types of products - Orthotics and Prosthetics.</h3>
-					</div>
-				</div>
+				<h3>We specialize in two main types of products - Orthotics and Prosthetics.</h3>
 			</div>
 		</div>
 	</div>
