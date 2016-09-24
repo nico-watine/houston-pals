220619d24d86e0985c68ba632f3ebade966c792e
Nico Watine <nico@studioncreations.com>
2016-09-24 08:37:10 -0500

    Tweaked blog page “coming soon” text

--- src/pages/blog.php
+++ src/pages/blog.php
@@ -2,7 +2,7 @@
 url: /blog
 title: Coming Soon - Blog
 description: Articles and guides on how to make the most of your pet's orthotic and prosthetic devices
-og-title: The Houston PALS Blog - Coming Soon
+og-title: The Houston PALS Blog - Articles Coming Soon
 og-description: Articles and guides on how to make the most of your pet's orthotic and prosthetic devices are coming soon
 og-image: /img/assets/og/1/og-image-1.jpg
 body-id: 
@@ -11,7 +11,7 @@ body-class: coming-soon
 <div class="row" id="top-row">
 	<div class="medium-push-1 medium-10 columns">
 		<h1 class="title">Blog</h1>
-		<h2>Coming Soon</h2>
+		<h2>Blog posts coming soon!</h2>
 	</div>
 </div>
 
