341ca7a0e44ab70d92284d7b5be6cbf9494c23d6
Nico Watine <nico@studioncreations.com>
2016-09-24 07:33:58 -0500

    Stash and delete branch

--- src/pages/ordering-product-faq.php
+++ src/pages/ordering-product-faq.php
@@ -5,13 +5,12 @@ description: The scoop on HPALS products and how to order
 og-title: Ordering & Product FAQ - Coming Soon
 og-description: The scoop on HPALS products and how to order yours is coming soon
 og-image: /img/assets/og/1/og-image-1.jpg
-body-id: 
-body-class: coming-soon
+body-id: ordering-product-faq
+body-class: 
 ---
 <div class="row" id="top-row">
 	<div class="medium-push-1 medium-10 columns">
 		<h1 class="title">Ordering + Product FAQ</h1>
-		<h2>Coming Soon</h2>
 	</div>
 </div>
 
