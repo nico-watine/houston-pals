961646e514fc8a66f44493f4e574f4a85d4a0724
Nico Watine <nico@studioncreations.com>
2016-09-24 07:39:19 -0500

    Ordering Product FAQ sass sheets set up
    They are duplicate structures of /products

--- src/pages/ordering-product-faq.php
+++ src/pages/ordering-product-faq.php
@@ -8,9 +8,17 @@ og-image: /img/assets/og/1/og-image-1.jpg
 body-id: ordering-product-faq
 body-class: 
 ---
-<div class="row" id="top-row">
-	<div class="medium-push-1 medium-10 columns">
-		<h1 class="title">Ordering + Product FAQ</h1>
+<div class="content-wrapper">
+	<div class="content no-paw">
+		<div class="row page-intro">
+			<div class="medium-push-1 medium-11 columns">
+				<h1 class="title">Ordering + Product FAQ</h1>
+				<div class="row">
+					<div class="medium-push-1 medium-11 columns">
+						<h3>We specialize in two main types of products - Orthotics and Prosthetics.</h3>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
-
