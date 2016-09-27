92d15424020c00af361ac37c78fe6c174fd332ba
Nico Watine <nico@studioncreations.com>
2016-09-27 10:12:24 -0500

    Created schema:person div
    Will now populate the div with all the included metadata

--- src/pages/index.php
+++ src/pages/index.php
@@ -16,20 +16,22 @@ body-class:
 		<img src="/img/index/dog-trio/1/dog-trio.jpg" srcset="/img/index/dog-trio/1/dog-trio_2x.jpg 2x" alt="PALS Dogs">
 	</div>
 </div>
-<div class="row" id="intro">
-	<div class="medium-5 columns text-center" id="profile-pic">
-		<h1 class="title show-for-small-only"><span>About</span> PALS</h1>
-		<img src="{{ cdn }}/img/about/bill-default.jpg" alt="Bill Bickley">
-	</div>
-	<div class="medium-7 large-6 columns end">
-		<div class="prose">
-			<h1 class="title show-for-medium"><span>About</span> PALS</h1>
-			<h2>In 2013, Bill Bickley, a licensed Orthotist and Prosthetist with over 20 years of experience, founded Pet Artificial Limbs and Supports (PALS) in Houston, Texas.</h2>
+<div itemscope itemtype="http://schema.org/Person">
+	<div class="row" id="intro">
+		<div class="medium-5 columns text-center" id="profile-pic">
+			<h1 class="title show-for-small-only"><span>About</span> PALS</h1>
+			<img src="{{ cdn }}/img/about/bill-default.jpg" alt="Bill Bickley">
+		</div>
+		<div class="medium-7 large-6 columns end">
+			<div class="prose">
+				<h1 class="title show-for-medium"><span>About</span> PALS</h1>
+				<h2>In 2013, Bill Bickley, a licensed Orthotist and Prosthetist with over 20 years of experience, founded Pet Artificial Limbs and Supports (PALS) in Houston, Texas.</h2>
+			</div>
 		</div>
 	</div>
-</div>
-<div class="row" id="story">
-	<div class="medium-10 medium-offset-1 large-8 large-offset-2 end columns">
-		<p>Bill wished to apply his twenty years of experience in custom design and fabrication, and his knowledge of specialized materials, to the world of animal medicine. Orthotics (supportive devices) &amp; Prosthetics (artificial limbs) are improving the lives of injured animals. These devices, when made by an experienced practitioner and individually designed for your animal's needs, can have a significant impact on both surgical outcome and post injury rehabilitation. Being a graduate of Northwestern University's Orthotic &amp; Prosthetic program, and having worked with both the rehabilitation of adults and children, Bill brings a vast amount of experience to the field of animal medicine.</p>
+	<div class="row" id="story">
+		<div class="medium-10 medium-offset-1 large-8 large-offset-2 end columns">
+			<p>Bill wished to apply his twenty years of experience in custom design and fabrication, and his knowledge of specialized materials, to the world of animal medicine. Orthotics (supportive devices) &amp; Prosthetics (artificial limbs) are improving the lives of injured animals. These devices, when made by an experienced practitioner and individually designed for your animal's needs, can have a significant impact on both surgical outcome and post injury rehabilitation. Being a graduate of Northwestern University's Orthotic &amp; Prosthetic program, and having worked with both the rehabilitation of adults and children, Bill brings a vast amount of experience to the field of animal medicine.</p>
+		</div>
 	</div>
 </div>
