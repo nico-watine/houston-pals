50959f310f84a2172fcc6bdb86a810687f8dc12e
Nico Watine <nico@studioncreations.com>
2016-09-30 10:46:55 -0500

    Got fairly good slider functioning
    Have the two thumbs which each can navigate (pull up) their specific big slider
    
    Can change big image with previous/next

--- js/src/scripts.js
+++ js/src/scripts.js
@@ -1,4 +1,15 @@
-$('.slider').slick();
+$('.slider-for').slick({
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+	slidesToShow: 2,
+	slidesToScroll: 1,
+	asNavFor: '.slider-for',
+	focusOnSelect: true
+});
+
 // This file is to show how 'app.js' is for importing all of the Foundation-specific files
 // 'scripts.js' is a placehold file for any other script files
 // 'scripts.js' is minified via codekit, not via 'npm run build'
