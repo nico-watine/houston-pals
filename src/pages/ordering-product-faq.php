d48d92e12b20ceb6679bcfa8d8319e7835e23652
Nico Watine <nico@studioncreations.com>
2017-08-21 11:21:24 -0500

    Copy over tooltip to orthotics

--- src/pages/ordering-product-faq.php
+++ src/pages/ordering-product-faq.php
@@ -39,7 +39,7 @@ body-class:
 				<img src="/img/orderingProductFAQ/orthotic.jpg" alt="Orthotic Reference Photo">
 			</div>
 			<div class="plaque">
-				<p class="margin-0">A standard orthotic has an estimated price range between $1200 and $1500</p>
+				<p class="margin-0">A standard orthotic has an estimated price range between $1200 and $1500 <span data-tooltip aria-haspopup="true" class="has-tip" data-disable-hover="false" tabindex="1" title="Additional charge for heavy duty designs, such as for high activity and large animals."><strong>*</strong></span></p>
 			</div>
 		</div>
 	</div>
