9459051487238ef153961ce6ea6695c4e2a729ae
Nico Watine <nico@studioncreations.com>
2017-08-22 12:00:47 -0500

    Fix typo in index og-title

--- src/pages/index.php
+++ src/pages/index.php
@@ -1,8 +1,8 @@
 ---
 url: /
-title: Pet Artifical Limbs & Supports
+title: Pet Artificial Limbs & Supports
 description: Custom made orthotics and prosthetics devices for your pet in Houston, TX, created by Bill Bickley
-og-title: Pet Artifical Limbs & Supports | Houston PALS
+og-title: Pet Artificial Limbs & Supports | Houston PALS
 og-description: Custom made orthotics and prosthetics devices for your pet in Houston, TX, created by Bill Bickley
 og-image: /img/assets/og/1/og-image-1.jpg
 body-id: index
