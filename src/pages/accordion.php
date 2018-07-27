8fe69fb6e27eea107ffede4fa7673bc3b71674d2
Nico Watine <nico@studioncreations.com>
2018-07-27 10:38:12 -0500

    Copy/paste for demo functioning accordion

--- src/pages/accordion.php
+++ src/pages/accordion.php
@@ -8,3 +8,30 @@ og-image: /img/assets/og/1/og-image-1.jpg
 body-id: accordion
 body-class: accordion
 ---
+<div class="row" style="padding: 40px 0;">
+	<h1 class="title text-center">Accordion</h1>
+</div>
+<div class="row">
+	<div class="columns">
+<ul class="accordion" data-accordion data-deep-link="true" data-update-history="true" data-deep-link-smudge="true" data-deep-link-smudge-delay="500" id="deeplinked-accordion">
+  <li class="accordion-item is-active" data-accordion-item>
+    <a href="#deeplink1" class="accordion-title">Accordion 1</a>
+    <div class="accordion-content" data-tab-content id="deeplink1">
+      Panel 1. Lorem ipsum dolor
+    </div>
+  </li>
+  <li class="accordion-item" data-accordion-item>
+    <a href="#deeplink2" class="accordion-title">Accordion 2</a>
+    <div class="accordion-content" data-tab-content id="deeplink2">
+      Panel 2. Lorem ipsum dolor
+    </div>
+  </li>
+  <li class="accordion-item" data-accordion-item>
+    <a href="#deeplink3" class="accordion-title">Accordion 3</a>
+    <div class="accordion-content" data-tab-content id="deeplink3">
+      Panel 3. Lorem ipsum dolor
+    </div>
+  </li>
+</ul>
+	</div>
+</div>
