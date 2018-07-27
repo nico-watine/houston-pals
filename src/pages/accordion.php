9df2fbfb53efaac13dc4bc13ce7750a1bae859e0
Nico Watine <nico@studioncreations.com>
2018-07-27 10:25:54 -0500

    New blank page to dev the accordion

---
+++ src/pages/accordion.php
@@ -0,0 +1,10 @@
+---
+url: /
+title: Pet Artificial Limbs & Supports
+description: Custom made orthotics and prosthetics devices for your pet in Houston, TX, created by Bill Bickley
+og-title: Pet Artificial Limbs & Supports | Houston PALS
+og-description: Custom made orthotics and prosthetics devices for your pet in Houston, TX, created by Bill Bickley
+og-image: /img/assets/og/1/og-image-1.jpg
+body-id: accordion
+body-class: accordion
+---
