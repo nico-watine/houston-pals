cfe488c9e4b61a5558d2ef97034c51e503d32792
Nico Watine <nico@studioncreations.com>
2018-07-30 14:06:23 -0500

    Basic copy/paste for icons

--- src/pages/accordion.php
+++ src/pages/accordion.php
@@ -14,6 +14,7 @@ body-class: accordion
 <div class="grid-x grid-padding-x" style="padding-bottom: 40px;">
 	<div class="cell">
 		<div class="imgWrap" style="max-width: 770px; width: 100%; margin: 0 auto;">
+			<img src="/img/product-builder/affected-limb/front/front.png" alt="" id="affected-limb">
 			<img src="/img/product-builder/dog/tan-dog.png" alt="" id="product-builder-model">
 		</div>
 	</div>
@@ -35,7 +36,10 @@ body-class: accordion
 			<li class="accordion-item" data-accordion-item>
 				<a href="#affected-limb" class="accordion-title">Affected Limb</a>
 				<div class="accordion-content" data-tab-content id="deeplink2">
-				Panel 2. Lorem ipsum dolor
+					<div class="grid-x">
+						<div class="cell small-3 text-center" style="padding: 0 5px;"><span onclick="getElementById('affected-limb').src=('/img/product-builder/dog/tan-dog.png')" style="display: inline-block; cursor: pointer;"><img src="/img/product-builder/coat-color/tan.png" alt=""><p>Tan</p></span></div>
+						<div class="cell small-3 text-center" style="padding: 0 5px;"><span onclick="getElementById('affected-limb').src=('/img/product-builder/dog/black-dog.png')" style="display: inline-block; cursor: pointer;"><img src="/img/product-builder/coat-color/black.png" alt=""><p>Black</p></span></div>
+					</div>
 				</div>
 			</li>
 			<li class="accordion-item" data-accordion-item>
