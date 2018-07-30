4fc8cbda38230ca21a1ef7fb0b19da04f0e97c1b
Nico Watine <nico@studioncreations.com>
2018-07-30 18:29:01 -0500

    Annotate functions

--- js/src/app.js
+++ js/src/app.js
@@ -13,18 +13,18 @@ $('.rear-affected-limb-toggle').click(function(){
 
 // Hide "white" Affected Limb after choosing Injury Solution
 
-	$('.front-injury-solution-option, .rear-injury-solution-option').click(function(){
-		$('#affected-limb-img').addClass('is-hidden');
-		$('#affected-limb-img').removeClass('is-visible');
-		$('#injury-solution-img').removeClass('is-hidden');
-		$('#injury-solution-img').addClass('is-visible');
-	});
+$('.front-injury-solution-option, .rear-injury-solution-option').click(function(){
+	$('#affected-limb-img').addClass('is-hidden');
+	$('#affected-limb-img').removeClass('is-visible');
+	$('#injury-solution-img').removeClass('is-hidden');
+	$('#injury-solution-img').addClass('is-visible');
+});
 
 // Hide chosen limb & product options when retroactively clicking on Affected Limb
 
-	$('.front-affected-limb-toggle, .rear-affected-limb-toggle').click(function(){
-		$('#injury-solution-img').removeClass('is-visible');
-		$('#injury-solution-img').addClass('is-hidden');
-		$('#affected-limb-img').addClass('is-visible');
-		$('#affected-limb-img').removeClass('is-hidden');
-	});
+$('.front-affected-limb-toggle, .rear-affected-limb-toggle').click(function(){
+	$('#injury-solution-img').removeClass('is-visible');
+	$('#injury-solution-img').addClass('is-hidden');
+	$('#affected-limb-img').addClass('is-visible');
+	$('#affected-limb-img').removeClass('is-hidden');
+});
