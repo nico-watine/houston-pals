13f77edd09186a9968ac32d44c924000197fcc62
Nico Watine <nico@studioncreations.com>
2018-08-02 18:46:15 -0500

    Add rear prosthetics

--- src/pages/accordion.php
+++ src/pages/accordion.php
@@ -224,6 +224,49 @@ body-class: accordion
 							<span onclick="getElementById('tailored-finish-img').src=('/img/product-builder/rear/orthosis/rear-orthosis-1201.png')"><img class="circle" src="/img/product-builder/tailored-finish/1201.png" alt=""><p>10</p></span>
 						</div>
 					</div>
+					<!-- Rear Prosthetic -->
+					<div class="is-hidden grid-x small-up-3 medium-up-5" id="rear-orthotic-tailored-finish-option">
+						<!-- 1013 -->
+						<div class="rear-orthotic-tailored-finish-option cell option-cell">
+							<span onclick="getElementById('tailored-finish-img').src=('/img/product-builder/rear/prosthesis/rear-prosthesis-1013.png')"><img class="circle" src="/img/product-builder/tailored-finish/1013.png" alt=""><p>1</p></span>
+						</div>
+						<!-- 1025 -->
+						<div class="rear-orthotic-tailored-finish-option cell option-cell">
+							<span onclick="getElementById('tailored-finish-img').src=('/img/product-builder/rear/prosthesis/rear-prosthesis-1025.png')"><img class="circle" src="/img/product-builder/tailored-finish/1025.png" alt=""><p>2</p></span>
+						</div>
+						<!-- 1026 -->
+						<div class="rear-orthotic-tailored-finish-option cell option-cell">
+							<span onclick="getElementById('tailored-finish-img').src=('/img/product-builder/rear/prosthesis/rear-prosthesis-1026.png')"><img class="circle" src="/img/product-builder/tailored-finish/1026.png" alt=""><p>3</p></span>
+						</div>
+						<!-- 1050 -->
+						<div class="rear-orthotic-tailored-finish-option cell option-cell">
+							<span onclick="getElementById('tailored-finish-img').src=('/img/product-builder/rear/prosthesis/rear-prosthesis-1050.png')"><img class="circle" src="/img/product-builder/tailored-finish/1050.png" alt=""><p>4</p></span>
+						</div>
+						<!-- 1053 -->
+						<div class="rear-orthotic-tailored-finish-option cell option-cell">
+							<span onclick="getElementById('tailored-finish-img').src=('/img/product-builder/rear/prosthesis/rear-prosthesis-1053.png')"><img class="circle" src="/img/product-builder/tailored-finish/1053.png" alt=""><p>5</p></span>
+						</div>
+						<!-- 1060 -->
+						<div class="rear-orthotic-tailored-finish-option cell option-cell">
+							<span onclick="getElementById('tailored-finish-img').src=('/img/product-builder/rear/prosthesis/rear-prosthesis-1060.png')"><img class="circle" src="/img/product-builder/tailored-finish/1060.png" alt=""><p>6</p></span>
+						</div>
+						<!-- 1071 -->
+						<div class="rear-orthotic-tailored-finish-option cell option-cell">
+							<span onclick="getElementById('tailored-finish-img').src=('/img/product-builder/rear/prosthesis/rear-prosthesis-1071.png')"><img class="circle" src="/img/product-builder/tailored-finish/1071.png" alt=""><p>7</p></span>
+						</div>
+						<!-- 1083 -->
+						<div class="rear-orthotic-tailored-finish-option cell option-cell">
+							<span onclick="getElementById('tailored-finish-img').src=('/img/product-builder/rear/prosthesis/rear-prosthesis-1083.png')"><img class="circle" src="/img/product-builder/tailored-finish/1083.png" alt=""><p>8</p></span>
+						</div>
+						<!-- 1090 -->
+						<div class="rear-orthotic-tailored-finish-option cell option-cell">
+							<span onclick="getElementById('tailored-finish-img').src=('/img/product-builder/rear/prosthesis/rear-prosthesis-1090.png')"><img class="circle" src="/img/product-builder/tailored-finish/1090.png" alt=""><p>9</p></span>
+						</div>
+						<!-- 1201 -->
+						<div class="rear-orthotic-tailored-finish-option cell option-cell">
+							<span onclick="getElementById('tailored-finish-img').src=('/img/product-builder/rear/prosthesis/rear-prosthesis-1201.png')"><img class="circle" src="/img/product-builder/tailored-finish/1201.png" alt=""><p>10</p></span>
+						</div>
+					</div>
 				</div>
 			</li>
 		</ul>
