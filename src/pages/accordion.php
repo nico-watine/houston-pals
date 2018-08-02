2c86a4971f74753950c243d1094f8e3324aca085
Nico Watine <nico@studioncreations.com>
2018-08-02 11:01:37 -0500

    New final img and reference

--- src/pages/accordion.php
+++ src/pages/accordion.php
@@ -58,7 +58,7 @@ body-class: accordion
 						<!-- Rear Orthotic -->
 						<div class="is-hidden rear-injury-solution-option cell small-6 text-center" style="padding: 0 5px;"><span onclick="getElementById('injury-solution-img').src=('/img/product-builder/injury-solution/rear/rear-orthotic.png')" style="display: inline-block; cursor: pointer;"><img src="/img/product-builder/injury-solution/rear/rear-orthotic-icon.png" alt=""><p>Rear Orthotic</p></span></div>
 						<!-- Rear Prosthetic -->
-						<div class="is-hidden rear-injury-solution-option cell small-6 text-center" style="padding: 0 5px;"><span onclick="getElementById('injury-solution-img').src=('/img/product-builder/injury-solution/rear/rear-prosthetic.png')" style="display: inline-block; cursor: pointer;"><img src="/img/product-builder/injury-solution/rear/rear-prosthesis-icon.png" alt=""><p>Rear Prosthetic</p></span></div>
+						<div class="is-hidden rear-injury-solution-option cell small-6 text-center" style="padding: 0 5px;"><span onclick="getElementById('injury-solution-img').src=('/img/product-builder/injury-solution/rear/rear-prosthesis.png')" style="display: inline-block; cursor: pointer;"><img src="/img/product-builder/injury-solution/rear/rear-prosthesis-icon.png" alt=""><p>Rear Prosthetic</p></span></div>
 					</div>
 				</div>
 			</li>
