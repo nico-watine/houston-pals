b2c87d4f44be86cbfaa8cd8e428cbadc92d8a8e0
Nico Watine <nico@studioncreations.com>
2018-08-04 08:20:59 -0400

    Annotations

--- js/src/app.js
+++ js/src/app.js
@@ -1,10 +1,10 @@
 $(document).foundation();
 
 // Toggle Injury Solution options based on Affected Limb chosen
-
-$('.front-affected-limb-toggle').click(function(){
-	$('.front-injury-solution-option').removeClass('is-hidden');
-	$('.rear-injury-solution-option').addClass('is-hidden');
+// Front
+$('.front-affected-limb-toggle').click(function(){ // Select "Front Affected Limb"
+	$('.front-injury-solution-option').removeClass('is-hidden'); // Show "Front Orthotic" and "Front Prosthetic"
+	$('.rear-injury-solution-option').addClass('is-hidden'); // Hide "Rear Orthotic" and "Rear Prosthetic"
 });
 $('.rear-affected-limb-toggle').click(function(){
 	$('.rear-injury-solution-option').removeClass('is-hidden');
