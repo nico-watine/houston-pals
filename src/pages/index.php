56484785c4c405b2f6077f46222ede295db1e036
Nico Watine <nico@studioncreations.com>
2018-08-04 08:04:18 -0400

    Insert correct link value

--- src/pages/index.php
+++ src/pages/index.php
@@ -65,7 +65,7 @@ layout: index
 <section>
 	<div class="grid-container fluid full">
 		<div class="grid-x">
-			<a class="cell medium-6" href="#">
+			<a class="cell medium-6" href="/accordion.php">
 			<div class="build-a-brace-block align-middle grid-x">
 					<div class="align-self-bottom content">
 						<div class="prose text-center medium-text-left">
