b0d767ac6200e53a6595d0eb18de55ce4d678b0a
Nico Watine <nico@studioncreations.com>
2018-08-07 10:16:01 -0400

    Move inlines to scss

--- src/pages/accordion.php
+++ src/pages/accordion.php
@@ -9,9 +9,9 @@ body-id: accordion
 body-class: accordion
 ---
 <section class="grid-x grid-padding-x page-intro">
-	<div class="cell medium-offset-1 shrink" style="padding-top: 40px; padding-bottom: 40px;">
+	<div class="cell medium-offset-1 shrink">
 		<h1 class="title" style="text-transform: none; letter-spacing: normal;"><a href="/accordion">Build-A-Brace</a></h1>
-		<div class="prose" style="max-width: 470px;">
+		<div class="prose">
 			<p>Use our guided build-a-brace process to generate a visual mock-up of your pet’s unique injury solution.</p>
 			<p>Each PALS-produced limb support is custom fitted to match your pet’s exact needs.</p>
 		</div>
