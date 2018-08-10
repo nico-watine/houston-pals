36035d9436397042ddf783e3f839abffe60db939
Nico Watine <nico@studioncreations.com>
2018-08-10 11:58:22 -0500

    Tweak visuals of link

--- src/pages/products.php
+++ src/pages/products.php
@@ -137,7 +137,7 @@ layout: products
 				<div class="columns medium-10 medium-push-1 large-8 large-push-2 end">
 					<div class="build-a-brace-callout">
 						<h2 class="title" style="text-transform: none; letter-spacing: normal;">Build-A-Brace Tool <span class="body-font h5 black">New!</span></h2>
-						<p>Generate your pet's injury solution design with our unique <a class="inline-block" style="display: inline-block;text-decoration: underline;" href="/build-a-brace.php"><strong>Build-A-Brace</strong></a> tool. Choose from 10 different options.</p>
+						<p>Generate your pet's injury solution design with our unique <a class="inline-block" style="display: inline-block;" href="/build-a-brace.php"><strong>Build-A-Brace</strong></a> tool. Choose from 10 different options.</p>
 					</div>
 				</div>
 			</div>
