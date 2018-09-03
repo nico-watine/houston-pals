5c384f6c55fcd78df7bc4cb2d5454f405112fd31
Nico Watine <nico@studioncreations.com>
2018-09-03 17:36:10 -0500

    Title changes

--- src/pages/ordering-product-faq.php
+++ src/pages/ordering-product-faq.php
@@ -56,7 +56,7 @@ body-class:
 	<div class="columns medium-10 medium-push-1 large-8 large-push-2 end">
 		<div class="build-a-brace-callout">
 			<h2 class="title" style="text-transform: none; letter-spacing: normal;">Build-A-Brace Tool <span class="body-font h5 black">New!</span></h2>
-			<p>Generate your pet's injury solution design with our unique <a class="inline-block" style="display: inline-block;" href="/build-a-brace.php"><strong>Build-A-Brace</strong></a> tool. Choose from 10 different options.</p>
+			<p>Generate your pet's injury solution design with our unique <a class="inline-block" style="display: inline-block;" href="/build-a-brace.php"><strong>PALS Canine Design Center</strong></a> tool. Choose from 10 different options.</p>
 		</div>
 	</div>
 </div>
