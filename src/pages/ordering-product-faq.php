914fb53ab587726922dc4d8cbff137ea82cf50d1
Nico Watine <nico@studioncreations.com>
2018-09-03 18:03:34 -0500

    Move pricing to where requested

--- src/pages/ordering-product-faq.php
+++ src/pages/ordering-product-faq.php
@@ -13,7 +13,7 @@ body-class:
 		<h1 class="title">Ordering Your Pet’s Device</h1>
 	</div>
 </div>
-<div class="row">
+<!-- <div class="row">
 	<div class="columns text-center">
 		<h2>Pricing</h2>
 	</div>
@@ -43,7 +43,7 @@ body-class:
 			</div>
 		</div>
 	</div>
-</div>
+</div> -->
 <div class="row designAdjustment">
 	<div class="wrapper medium-2 medium-push-1 large-push-2 large-2 columns text-center marginBottom-20">
 		<img style="max-width: 170px;" src="/img/orderingProductFAQ/pawIcon.svg" alt="Paw Icon">
@@ -95,5 +95,36 @@ body-class:
 		<p class="bill-title"><em>Licensed Orthotist &amp; Prosthetist</em></p>
 		<a href="mailto:bill@houstonpals.com" class="email">bill@houstonpals.com</a>
 		<p class="phone">(832) 444-8384</p>
+	</div>
+</div>
+<div class="row">
+	<div class="columns text-center">
+		<h2>Pricing</h2>
+	</div>
+</div>
+<div class="row">
+	<div class="medium-6 large-5 large-push-1 columns">
+		<div class="priceCard marginBottom-20 x2">
+			<div class="priceCard-header">
+				<h4>Prosthetics</h4>
+				<div class="image-scrim">&nbsp;</div>
+				<img src="/img/orderingProductFAQ/prosthetic.jpg" alt="Prosthetic Reference Photo">
+			</div>
+			<div class="plaque">
+				<p class="margin-0">A standard prosthetic has an estimated price range between $1200 and $1500 <span data-tooltip aria-haspopup="true" class="has-tip" data-disable-hover="false" tabindex="1" title="Additional charge for heavy duty designs, such as for high activity and large animals."><strong>*</strong></span></p>
+			</div>
+		</div>
+	</div>
+	<div class="medium-6 large-5 large-push-1 columns end">
+		<div class="priceCard marginBottom-20 x2">
+			<div class="priceCard-header">
+				<h4>Orthotics</h4>
+				<div class="image-scrim">&nbsp;</div>
+				<img src="/img/orderingProductFAQ/orthotic.jpg" alt="Orthotic Reference Photo">
+			</div>
+			<div class="plaque">
+				<p class="margin-0">A standard orthotic has an estimated price range between $1200 and $1500 <span data-tooltip aria-haspopup="true" class="has-tip" data-disable-hover="false" tabindex="1" title="Additional charge for heavy duty designs, such as for high activity and large animals."><strong>*</strong></span></p>
+			</div>
+		</div>
 	</div>
 </div>
