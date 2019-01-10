45dadd58875df6a3d6ffc83d2a079c450526fbf5
Nico Watine <nico@studioncreations.com>
2019-01-09 22:20:23 -0600

    Add svg script into file

--- js/app-min.js
+++ js/app-min.js
@@ -1,1 +1,86 @@
-$(document).foundation(),$(".front-affected-limb-toggle, .rear-affected-limb-toggle").click(function(){$("#injury-solution-notification").addClass("is-hidden"),$("#tailored-finish-prechoice-samples").removeClass("is-hidden").addClass("is-visible"),$("#tailored-finish-img, #injury-solution-img").removeClass("is-visible").addClass("is-hidden"),$(".tailored-finish-options").removeClass("is-visible").addClass("is-hidden"),$("#affected-limb-img").removeClass("is-hidden").addClass("is-visible")}),$(".front-affected-limb-toggle").click(function(){$(".front-injury-solution-option").removeClass("is-hidden"),$(".rear-injury-solution-option").addClass("is-hidden")}),$(".rear-affected-limb-toggle").click(function(){$(".rear-injury-solution-option").removeClass("is-hidden"),$(".front-injury-solution-option").addClass("is-hidden")}),$(".front-injury-solution-option, .rear-injury-solution-option").click(function(){$("#affected-limb-img").removeClass("is-visible").addClass("is-hidden")}),$(".front-injury-solution-option, .rear-injury-solution-option").click(function(){$("#injury-solution-img").removeClass("is-hidden").addClass("is-visible")}),$(".front-injury-solution-option, .rear-injury-solution-option").click(function(){$("#tailored-finish-prechoice-samples").removeClass("is-visible").addClass("is-hidden")}),$("#front-injury-orthotic-solution").click(function(){$(".tailored-finish-options").addClass("is-hidden"),$(".tailored-finish-options#front-orthotic-tailored-finish-option").removeClass("is-hidden")}),$("#front-injury-prosthetic-solution").click(function(){$(".tailored-finish-options").addClass("is-hidden"),$(".tailored-finish-options#front-prosthetic-tailored-finish-option").removeClass("is-hidden")}),$("#rear-injury-orthotic-solution").click(function(){$(".tailored-finish-options").addClass("is-hidden"),$(".tailored-finish-options#rear-orthotic-tailored-finish-option").removeClass("is-hidden")}),$("#rear-injury-prosthetic-solution").click(function(){$(".tailored-finish-options").addClass("is-hidden"),$(".tailored-finish-options#rear-prosthetic-tailored-finish-option").removeClass("is-hidden")}),$("#front-injury-orthotic-solution, #front-injury-prosthetic-solution, #rear-injury-orthotic-solution, #rear-injury-prosthetic-solution").click(function(){$("#tailored-finish-img").removeClass("is-hidden").addClass("is-visible")}),$("#tan-coat").click(function(){$("#tan-coat").addClass("active"),$("#black-coat, #brown-coat, #mixed-coat").removeClass("active")}),$("#black-coat").click(function(){$("#black-coat").addClass("active"),$("#brown-coat, #tan-coat, #mixed-coat").removeClass("active")}),$("#brown-coat").click(function(){$("#brown-coat").addClass("active"),$("#black-coat, #tan-coat, #mixed-coat").removeClass("active")}),$("#mixed-coat").click(function(){$("#mixed-coat").addClass("active"),$("#black-coat, #tan-coat, #brown-coat").removeClass("active")}),$("#front-limb").click(function(){$("#front-limb").addClass("active"),$("#rear-limb, #front-prosthesis, #rear-orthosis, #rear-prosthesis, #front-orthosis").removeClass("active")}),$("#rear-limb").click(function(){$("#rear-limb").addClass("active"),$("#front-limb, #front-prosthesis, #rear-orthosis, #rear-prosthesis, #front-orthosis").removeClass("active")}),$("#front-orthosis").click(function(){$("#front-orthosis").addClass("active"),$("#front-prosthesis, #rear-orthosis, #rear-prosthesis").removeClass("active")}),$("#front-prosthesis").click(function(){$("#front-prosthesis").addClass("active"),$("#front-orthosis, #rear-orthosis, #rear-prosthesis").removeClass("active")}),$("#rear-orthosis").click(function(){$("#rear-orthosis").addClass("active"),$("#rear-prosthesis, #front-orthosis, #front-prosthesis").removeClass("active")}),$("#rear-prosthesis").click(function(){$("#rear-prosthesis").addClass("active"),$("#rear-orthosis, #front-orthosis, #front-prosthesis").removeClass("active")});
+$(document).foundation(),
+// ----------------------------------------------------------------------
+// Resetting the Builder ------------------------------------------------
+// ----------------------------------------------------------------------
+// Selecting "Front Limb" or "Rear Limb"
+// ----------------------------------------------------------------------
+$(".front-affected-limb-toggle, .rear-affected-limb-toggle").click(function(){$("#injury-solution-notification").addClass("is-hidden"),// Remove "Please specify the Affected Limb first." after choosing Front or Rear limb
+$("#tailored-finish-prechoice-samples").removeClass("is-hidden").addClass("is-visible"),// Show prechoice samples
+$("#tailored-finish-img, #injury-solution-img").removeClass("is-visible").addClass("is-hidden"),// Hide colored & gray device image
+$(".tailored-finish-options").removeClass("is-visible").addClass("is-hidden"),// Hide colored finish options
+$("#affected-limb-img").removeClass("is-hidden").addClass("is-visible")}),
+// --------------------------------------------------------------
+// Step #1 ------------------------------------------------------
+// --------------------------------------------------------------
+// Toggle Injury Solution options based on Affected Limb chosen
+// --------------------------------------------------------------
+// Front
+$(".front-affected-limb-toggle").click(function(){// Select "Front Affected Limb"
+$(".front-injury-solution-option").removeClass("is-hidden"),// Show "Front Orthotic" and "Front Prosthetic"
+$(".rear-injury-solution-option").addClass("is-hidden")}),
+// Rear
+$(".rear-affected-limb-toggle").click(function(){// Select "Rear Affected Limb"
+$(".rear-injury-solution-option").removeClass("is-hidden"),// Hide "Front Orthotic" and "Front Prosthetic"
+$(".front-injury-solution-option").addClass("is-hidden")}),
+// -------------------------------------------------------------------------------------
+// Step #2 -----------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------
+// Hide "white" Affected Limb after choosing Injury Solution ("Orthotic" or "Prosthetic")
+// -------------------------------------------------------------------------------------
+$(".front-injury-solution-option, .rear-injury-solution-option").click(function(){$("#affected-limb-img").removeClass("is-visible").addClass("is-hidden")}),
+// ----------------------------------------------------------------------------
+// Step #4 --------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+// Show gray "Orthotic" or "Prosthetic" device on chosen limb
+// ----------------------------------------------------------------------------
+$(".front-injury-solution-option, .rear-injury-solution-option").click(function(){$("#injury-solution-img").removeClass("is-hidden").addClass("is-visible");// Show gray "Orthotic" or "Prosthetic" device
+}),
+// ----------------------------------------------------------------------------
+// Step #5 --------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+// Hide "Please specify the Injury Solution." & Tailored Finish example options
+// ----------------------------------------------------------------------------
+$(".front-injury-solution-option, .rear-injury-solution-option").click(function(){$("#tailored-finish-prechoice-samples").removeClass("is-visible").addClass("is-hidden")}),
+// ------------------------------------------------------------------------------------
+// Step #6 ----------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------
+// Display "Tailored Options" and "Tailored Finish Image" based on chosen Limb & Device
+// ------------------------------------------------------------------------------------
+// Front - Orthotic
+$("#front-injury-orthotic-solution").click(function(){$(".tailored-finish-options").addClass("is-hidden"),$(".tailored-finish-options#front-orthotic-tailored-finish-option").removeClass("is-hidden")}),
+// Front - Prosthetic
+$("#front-injury-prosthetic-solution").click(function(){$(".tailored-finish-options").addClass("is-hidden"),$(".tailored-finish-options#front-prosthetic-tailored-finish-option").removeClass("is-hidden")}),
+// Rear - Orthotic
+$("#rear-injury-orthotic-solution").click(function(){$(".tailored-finish-options").addClass("is-hidden"),$(".tailored-finish-options#rear-orthotic-tailored-finish-option").removeClass("is-hidden")}),
+// Rear - Prosthetic
+$("#rear-injury-prosthetic-solution").click(function(){$(".tailored-finish-options").addClass("is-hidden"),$(".tailored-finish-options#rear-prosthetic-tailored-finish-option").removeClass("is-hidden")}),
+// -------------------------------------------------------------
+// Step #7 -----------------------------------------------------
+// -------------------------------------------------------------
+// Display "Tailored Finish Image" based on chosen Limb & Device
+// -------------------------------------------------------------
+$("#front-injury-orthotic-solution, #front-injury-prosthetic-solution, #rear-injury-orthotic-solution, #rear-injury-prosthetic-solution").click(function(){$("#tailored-finish-img").removeClass("is-hidden").addClass("is-visible");// Show "colored" device
+}),
+// ------------------------------------------------------------------------------------
+// Toggling .active to spans when clicked
+// ------------------------------------------------------------------------------------
+// Coat-Color
+$("#tan-coat").click(function(){$("#tan-coat").addClass("active"),$("#black-coat, #brown-coat, #mixed-coat").removeClass("active")}),$("#black-coat").click(function(){$("#black-coat").addClass("active"),$("#brown-coat, #tan-coat, #mixed-coat").removeClass("active")}),$("#brown-coat").click(function(){$("#brown-coat").addClass("active"),$("#black-coat, #tan-coat, #mixed-coat").removeClass("active")}),$("#mixed-coat").click(function(){$("#mixed-coat").addClass("active"),$("#black-coat, #tan-coat, #brown-coat").removeClass("active")}),
+// Affected Limb
+$("#front-limb").click(function(){$("#front-limb").addClass("active"),$("#rear-limb, #front-prosthesis, #rear-orthosis, #rear-prosthesis, #front-orthosis").removeClass("active")}),$("#rear-limb").click(function(){$("#rear-limb").addClass("active"),$("#front-limb, #front-prosthesis, #rear-orthosis, #rear-prosthesis, #front-orthosis").removeClass("active")}),
+// Injury Solution
+$("#front-orthosis").click(function(){$("#front-orthosis").addClass("active"),$("#front-prosthesis, #rear-orthosis, #rear-prosthesis").removeClass("active")}),$("#front-prosthesis").click(function(){$("#front-prosthesis").addClass("active"),$("#front-orthosis, #rear-orthosis, #rear-prosthesis").removeClass("active")}),$("#rear-orthosis").click(function(){$("#rear-orthosis").addClass("active"),$("#rear-prosthesis, #front-orthosis, #front-prosthesis").removeClass("active")}),$("#rear-prosthesis").click(function(){$("#rear-prosthesis").addClass("active"),$("#rear-orthosis, #front-orthosis, #front-prosthesis").removeClass("active")}),
+/*
+* Replace all SVG images with inline SVG
+*/
+jQuery("img.svg").each(function(){var s=jQuery(this),t=s.attr("id"),r=s.attr("class"),i=s.attr("src");jQuery.get(i,function(i){
+// Get the SVG tag, ignore the rest
+var o=jQuery(i).find("svg");
+// Add replaced image's ID to the new SVG
+void 0!==t&&(o=o.attr("id",t)),
+// Add replaced image's classes to the new SVG
+void 0!==r&&(o=o.attr("class",r+" replaced-svg")),
+// Remove any invalid XML tags as per http://validator.w3.org
+o=o.removeAttr("xmlns:a"),
+// Replace image with new SVG
+s.replaceWith(o)},"xml")});
