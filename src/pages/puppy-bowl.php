ee8f0a71d4c1b0ecccd40e14eb719bdcbaf1f5be
Nico Watine <nico@studioncreations.com>
2019-01-24 01:10:53 -0600

    Add section image assets and start page

---
+++ src/pages/puppy-bowl.php
@@ -0,0 +1,16 @@
+---
+url: /ordering-product-faq
+title: Ordering & Product FAQ
+description: Depending on your animal’s needs, the cast design will be adjusted appropriately.
+og-title: Ordering & Product FAQ
+og-description: Depending on your animal’s needs, the cast design will be adjusted appropriately.
+og-image: /img/assets/og/1/og-image-1.jpg
+body-id: ordering-product-faq
+body-class:
+layout: blank
+---
+<section class="puppy-bowl">
+	<div class="row" style="padding: 0 40px;">
+
+	</div>
+</section>
