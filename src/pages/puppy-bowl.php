0b18a217da6ffaf91f054ea740161ff4af843bba
Nico Watine <nico@studioncreations.com>
2019-01-24 01:46:11 -0600

    .tag inlines moved to scss

--- src/pages/puppy-bowl.php
+++ src/pages/puppy-bowl.php
@@ -10,12 +10,10 @@ body-class:
 layout: blank
 ---
 <section class="puppy-bowl-section">
-	<div class="section-interior" style="background: rgba(48,101,155,0.80); padding: 40px 0;">
+	<div class="section-interior py-40">
 		<div class="row">
 			<div class="columns text-center">
-				<div>
-					<h4 class="display-inline-block" style="padding: 4px 28px; background: #F7F7F7; border-radius: 4px; text-transform: uppercase; line-height: 1; letter-spacing: 0.5px; margin-bottom: 24px">Upcoming!</h4>
-				</div>
+				<h4 class="display-inline-block tag">Upcoming!</h4>
 				<h3 style="color: #fff;"><strong>Animal Planet’s <br><span style="text-transform: uppercase; letter-spacing: 1px;" class="h2 heading">Puppy Bowl 15</span></strong></h3>
 				<h4 style="color: #fff;"><strong>February 3, 2019</strong></h4>
 				<br>
