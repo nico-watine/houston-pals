b79d06c2ebdfc73f147e1116995c905338352fcb
Nico Watine <nico@studioncreations.com>
2019-01-24 02:25:40 -0600

    Begin section w Will’s image

---
+++ src/pages/n/meet-will.php
@@ -0,0 +1,20 @@
+---
+url: /ordering-product-faq
+title: Ordering & Product FAQ
+description: Depending on your animal’s needs, the cast design will be adjusted appropriately.
+og-title: Ordering & Product FAQ
+og-description: Depending on your animal’s needs, the cast design will be adjusted appropriately.
+og-image: /img/assets/og/1/og-image-1.jpg
+body-id:
+body-class:
+layout: blank
+---
+<section class="meet-will py-40" style="background: #fff;">
+	<div class="grid-container">
+		<div class="grid-x grid-padding-x align-middle">
+			<div class="medium-5 cell text-right">
+				<img src="/img/index/meet-will/puppy-bowl-meet-will.jpg" style="max-width: 370px;" alt="">
+			</div>
+		</div>
+	</div>
+</section>
