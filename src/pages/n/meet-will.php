d0f5cff0d92f762e6798bbc23ee36f2840dae837
Nico Watine <nico@studioncreations.com>
2019-01-24 02:34:51 -0600

    Markup for @large completed

--- src/pages/n/meet-will.php
+++ src/pages/n/meet-will.php
@@ -9,11 +9,19 @@ body-id:
 body-class:
 layout: blank
 ---
-<section class="meet-will py-40" style="background: #fff;">
+<section class="meet-will py-40">
 	<div class="grid-container">
 		<div class="grid-x grid-padding-x align-middle">
 			<div class="medium-5 cell text-right">
-				<img src="/img/index/meet-will/puppy-bowl-meet-will.jpg" style="max-width: 370px;" alt="">
+				<img src="/img/index/meet-will/puppy-bowl-meet-will.jpg" style="border-radius: 24px; max-width: 370px;" alt="">
+			</div>
+			<div class="medium-7 cell">
+				<div class="prose-container p-16" style="background: #FEFEFE;border: 2px solid #30659B;border-radius: 16px; max-width: 600px;">
+					<h2 class="heading mb-32">Meet Will</h2>
+					<p class="lead mb-24">We’ve partnered with <strong>Doodle Rock Rescue</strong> in Dallas to rehabilitate Will and help him find a new home. On February 3, watch Will and his pals play in <strong>Animal Planet’s Puppy Bowl</strong> to spread adoption awareness.</p>
+					<p class="lead mb-32">Learn about Will’s inspiring story as he journeys towards health and home.</p>
+					<a href="#" class="display-inline-block h3 color-white py-16 px-40" style="background: #30659B;border: 2px solid #FFFFFF;box-shadow: 0 1px 4px 0 rgba(0,0,0,0.37);border-radius: 16px;line-height: 1;color:#fff;">Read It</a>
+				</div>
 			</div>
 		</div>
 	</div>
