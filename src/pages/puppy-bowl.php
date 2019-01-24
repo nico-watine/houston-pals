baab84d2fb734aaf409415c706bd7311e5086432
Nico Watine <nico@studioncreations.com>
2019-01-24 01:21:11 -0600

    Background and img begun

--- src/pages/puppy-bowl.php
+++ src/pages/puppy-bowl.php
@@ -5,12 +5,16 @@ description: Depending on your animal’s needs, the cast design will be adjuste
 og-title: Ordering & Product FAQ
 og-description: Depending on your animal’s needs, the cast design will be adjusted appropriately.
 og-image: /img/assets/og/1/og-image-1.jpg
-body-id: ordering-product-faq
+body-id:
 body-class:
 layout: blank
 ---
-<section class="puppy-bowl">
-	<div class="row" style="padding: 0 40px;">
-
+<section class="puppy-bowl-section">
+	<div class="section-interior" style="background: rgba(48,101,155,0.80); padding: 40px 0;">
+		<div class="row">
+			<div class="columns text-center">
+				<img src="/img/puppy-bowl/puppy-bowl-logo.png" style="width: 350px;" alt="">
+			</div>
+		</div>
 	</div>
 </section>
