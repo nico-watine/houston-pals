21d8ea3919dd21a44c5f3b8db422ecd14a7d16cf
Nico Watine <nico@studioncreations.com>
2019-01-24 02:04:45 -0600

    Update font-size specification method

--- src/pages/puppy-bowl.php
+++ src/pages/puppy-bowl.php
@@ -13,9 +13,9 @@ layout: blank
 	<div class="section-interior py-40">
 		<div class="row">
 			<div class="columns text-center">
-				<h4 class="display-inline-block tag">Upcoming!</h4>
-				<h3 class="announcement color-white"><strong>Animal Planet’s <br><span style="letter-spacing: 1px;" class="h2 heading uppercase">Puppy Bowl 15</span></strong></h3>
-				<h4 class="color-white"><strong>February 3, 2019</strong></h4>
+				<h4 class="h4 display-inline-block tag">Upcoming!</h4>
+				<h3 class="h3 announcement color-white"><strong>Animal Planet’s <br><span style="letter-spacing: 1px;" class="h2 heading uppercase">Puppy Bowl 15</span></strong></h3>
+				<h4 class="h4 color-white"><strong>February 3, 2019</strong></h4>
 				<br>
 			</div>
 		</div>
