3cc062ad340e22a8fac89fe9a946c908e2180240
Nico Watine <nico@studioncreations.com>
2019-01-25 09:56:46 -0600

    Insert and style button

--- src/pages/n/go-fund-me.php
+++ src/pages/n/go-fund-me.php
@@ -9,3 +9,18 @@ body-id:
 body-class:
 layout: blank
 ---
+<section class="go-fund-me-section" style="padding: 60px 0; border-top: 10px solid #30659b; border-bottom: 10px solid #30659b;">
+	<div class="grid-container">
+		<div class="grid-x grid-padding-x">
+			<div class="medium-6 cell">
+				<img src="/img/index/go-fund-me/video-holder.png" alt="">
+			</div>
+			<div class="medium-6 cell">
+				<p class="mb-24"><span class="lead"><strong>Bill has a passion for helping animals get back on their feet,</strong></span> and the advanced injury solutions that he provides are no walk in the park. He never wants the price of his services, however, to prevent animals from having their days in the park again.</p>
+				<p class="mb-24"><span class="lead heading">Kindly consider</span> a contribution to PALS.</p>
+				<p style="width: 420px;">Bill puts contributions towards his costs, allowing him to provide tailored treatment solutions at a lower price.</p>
+				<a href="#" class="button m-0 go-fund-me-page-link display-inline-block heading py-16 px-40"><img src="/img/index/go-fund-me/go-fund-me-logo.png" style="width: 165px;" alt=""></a>
+			</div>
+		</div>
+	</div>
+</section>
