b9d393cf29d00f72faa036e751ab559fc562df2f
Nico Watine <nico@studioncreations.com>
2019-01-25 09:59:24 -0600

    Tweak classes on button

--- src/pages/n/go-fund-me.php
+++ src/pages/n/go-fund-me.php
@@ -19,7 +19,7 @@ layout: blank
 				<p class="mb-24"><span class="lead"><strong>Bill has a passion for helping animals get back on their feet,</strong></span> and the advanced injury solutions that he provides are no walk in the park. He never wants the price of his services, however, to prevent animals from having their days in the park again.</p>
 				<p class="mb-24"><span class="lead heading">Kindly consider</span> a contribution to PALS.</p>
 				<p style="width: 420px;">Bill puts contributions towards his costs, allowing him to provide tailored treatment solutions at a lower price.</p>
-				<a href="#" class="button m-0 go-fund-me-page-link display-inline-block heading py-16 px-40"><img src="/img/index/go-fund-me/go-fund-me-logo.png" style="width: 165px;" alt=""></a>
+				<a href="#" class="button m-0 go-fund-me-page-link"><img src="/img/index/go-fund-me/go-fund-me-logo.png" style="width: 165px;" title="GoFundMe page link" alt=""></a>
 			</div>
 		</div>
 	</div>
