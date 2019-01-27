48799f638189995b191b9d2b5f856fdd737fedbb
Nico Watine <nico@studioncreations.com>
2019-01-27 12:34:09 -0600

    Setting widths

--- src/pages/n/go-fund-me.php
+++ src/pages/n/go-fund-me.php
@@ -12,10 +12,12 @@ layout: blank
 <section class="go-fund-me-section" style="padding: 60px 0; border-top: 10px solid #30659b; border-bottom: 10px solid #30659b;">
 	<div class="grid-container">
 		<div class="grid-x grid-padding-x">
-			<div class="medium-6 cell">
-				<img class="mb-40" src="/img/index/go-fund-me/video-holder.png" alt="">
+			<div class="large-6 cell">
+				<div class="pals-promo-video">
+					<img class="mb-40" src="/img/index/go-fund-me/video-holder.png" alt="">
+				</div>
 			</div>
-			<div class="medium-6 cell">
+			<div class="large-6 cell">
 				<article class="go-fund-me-article">
 					<p class="mb-24"><span class="lead"><strong>Bill has a passion for helping animals get back on their feet,</strong></span> and the advanced injury solutions that he provides are no walk in the park. He never wants the price of his services, however, to prevent animals from having their days in the park again.</p>
 					<p class="mb-24"><span class="lead heading">Kindly consider</span> a contribution to PALS.</p>
