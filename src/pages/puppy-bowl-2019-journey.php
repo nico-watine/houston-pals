46dcecaf2bc78d27dabf5615f558b4543b55a51f
Nico Watine <nico@studioncreations.com>
2019-01-28 20:32:25 -0600

    Get a basic 1st row set up

--- src/pages/puppy-bowl-2019-journey.php
+++ src/pages/puppy-bowl-2019-journey.php
@@ -9,3 +9,29 @@ body-id:
 body-class:
 layout: blank
 ---
+{{> header}}
+
+<main>
+	<div class="content-wrapper">
+		<div class="content">
+			<div class="grid-container">
+				<div class="grid-x grid-padding-x">
+					<div class="cell">
+						<div class="grid-x grid-padding-x align-center">
+							<div class="cell shrink">
+								<img src="/img/partners/doodle-rock-rescue.png" style="width: 300px;" alt="">
+							</div>
+							<div class="cell shrink">
+								<div class="prose" style="width: 570px; height: 200px; background: red;">
+
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</main>
+
+{{> footer}}
