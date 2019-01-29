2cdb9a393b6e2c534e1ce31d600582f1aedd22c5
Nico Watine <nico@studioncreations.com>
2019-01-28 22:05:44 -0600

    320 done

--- src/pages/puppy-bowl-2019-journey.php
+++ src/pages/puppy-bowl-2019-journey.php
@@ -20,12 +20,12 @@ layout: blank
 						<div class="cell">
 							<div class="grid-x grid-padding-x align-center align-middle">
 								<div class="cell medium-shrink">
-									<div class="doodle-rock-rescue-img img-wrap">
+									<div class="doodle-rock-rescue-img img-wrap text-center">
 										<img src="/img/partners/doodle-rock-rescue.png" alt="">
 									</div>
 								</div>
 								<div class="cell medium-shrink">
-									<div class="prose">
+									<div class="prose pb-16">
 										<h3 class="lead-heading"><strong>Dallas-based Doodle Rock Rescue reached out to us in Houston to assist Will in obtaining a limb.</strong></h3>
 										<p>Bill, the man behind <strong>Pet Artificial Limbs and Supports</strong>, fell in love with Will during his initial mold casting, and brought him home to foster him while making his new limb.</p>
 									</div>
@@ -35,7 +35,7 @@ layout: blank
 						<div class="cell">
 							<div class="grid-x grid-padding-x align-center align-middle">
 								<div class="cell hide-for-medium">
-									<div class="grid-x grid-padding-x align-middle">
+									<div class="grid-x grid-padding-x align-middle mb-32">
 										<div class="cell small-6">
 											<img src="/img/partners/animal-planet-logo.png" alt="">
 										</div>
@@ -43,11 +43,10 @@ layout: blank
 											<img src="/img/index/puppy-bowl/puppy-bowl-logo.png" alt="">
 										</div>
 									</div>
-									<br>
 								</div>
 								<div class="cell medium-shrink">
 									<div class="prose">
-										<p>Because of the success of Bill’s injury-solution limb for Will, <strong>Animal Planet</strong> reached out, asking for Will’s participation in <strong>Puppy Bowl 2019</strong>! Will, weighing a healthy 50 pounds, was too large to travel in the cabin of an airplane, so he and Bill set off on an exciting road trip to get him to New York, in time for the filming of Puppy Bowl 2019.</p>
+										<p class="mb-32">Because of the success of Bill’s injury-solution limb for Will, <strong>Animal Planet</strong> reached out, asking for Will’s participation in <strong>Puppy Bowl 2019</strong>! Will, weighing a healthy 50 pounds, was too large to travel in the cabin of an airplane, so he and Bill set off on an exciting road trip to get him to New York, in time for the filming of Puppy Bowl 2019.</p>
 									</div>
 								</div>
 								<div class="cell show-for-medium medium-shrink">
