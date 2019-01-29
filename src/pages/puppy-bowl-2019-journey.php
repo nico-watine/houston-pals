62250ce41cbed79859949aa60412fc59f1de7e4d
Nico Watine <nico@studioncreations.com>
2019-01-28 22:59:48 -0600

    Callout wrap done, now vertically place it

--- src/pages/puppy-bowl-2019-journey.php
+++ src/pages/puppy-bowl-2019-journey.php
@@ -29,7 +29,7 @@ layout: blank
 		<div class="content">
 			<div class="grid-container">
 				<section class="puppy-bowl-2019-journey-story">
-					<div class="grid-x grid-padding-x">
+					<div class="grid-x grid-padding-x align-center">
 						<div class="cell">
 							<div class="grid-x grid-padding-x align-center align-middle">
 								<div class="cell medium-shrink">
@@ -68,6 +68,20 @@ layout: blank
 									</div>
 								</div>
 							</div>
+						</div>
+						<div class="cell medium-shrink blog-link-callout">
+							<!-- <div class="blog-link-callout"> -->
+								<div class="grid-x grid-padding-x align-middle align-center">
+									<div class="cell shrink">
+										<div class="heading-wrap" style="max-width: 250px;">
+											<h4 class="heading color-white medium-text-right">The full story of their journey is detailed on our PALS blog.</h4>
+										</div>
+									</div>
+									<div class="cell shrink">
+										<a href="#" class="button m-0 blog-link display-inline-block heading py-16 px-40">Read It</a>
+									</div>
+								</div>
+							<!-- </div> -->
 						</div>
 					</div>
 				</section>
