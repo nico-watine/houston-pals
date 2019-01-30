f9755746f04230cd55aef155891ae61f22b50d54
Nico Watine <nico@studioncreations.com>
2019-01-30 09:25:38 -0600

    Add anchor links, incl alts & titles, to page

--- src/pages/puppy-bowl-2019-journey.php
+++ src/pages/puppy-bowl-2019-journey.php
@@ -34,12 +34,14 @@ layout: blank
 							<div class="grid-x grid-padding-x align-center align-middle">
 								<div class="cell medium-shrink">
 									<div class="doodle-rock-rescue-img img-wrap text-center">
-										<img src="/img/partners/doodle-rock-rescue.png" alt="">
+										<a href="https://www.doodlerockrescue.org/" target="_blank" title="doodlerockrescue.org link">
+											<img src="/img/partners/doodle-rock-rescue.png" alt="Doodle Rock Rescue logo">
+										</a>
 									</div>
 								</div>
 								<div class="cell medium-shrink">
 									<div class="prose pb-16">
-										<h3 class="lead-heading"><strong>Dallas-based Doodle Rock Rescue reached out to us in Houston to assist Will in obtaining a limb.</strong></h3>
+										<h3 class="lead-heading"><strong>Dallas-based <a href="https://www.doodlerockrescue.org/" target="_blank" title="doodlerockrescue.org link">Doodle Rock Rescue</a> reached out to us in Houston to assist Will in obtaining a limb.</strong></h3>
 										<p>Bill, the man behind <strong>Pet Artificial Limbs and Supports</strong>, fell in love with Will during his initial mold casting, and brought him home to foster him while making his new limb.</p>
 									</div>
 								</div>
@@ -50,21 +52,27 @@ layout: blank
 								<div class="cell double-img-row">
 									<div class="grid-x grid-padding-x align-middle mb-32">
 										<div class="cell small-6">
-											<img src="/img/partners/animal-planet-logo.png" alt="">
+											<a href="https://www.animalplanet.com/" target="_blank" title="animalplanet.com link">
+												<img src="/img/partners/animal-planet-logo.png" alt="Animal Planet logo">
+											</a>
 										</div>
 										<div class="cell small-6">
-											<img src="/img/index/puppy-bowl/puppy-bowl-logo.png" alt="">
+											<a href="https://www.animalplanet.com/tv-shows/puppy-bowl/" target="_blank" title="Puppy Bowl 2019 website link">
+												<img src="/img/index/puppy-bowl/puppy-bowl-logo.png" alt="Puppy Bowl 2019 logo">
+											</a>
 										</div>
 									</div>
 								</div>
 								<div class="cell medium-shrink">
 									<div class="prose">
-										<p class="mb-32">Because of the success of Bill’s injury-solution limb for Will, <strong>Animal Planet</strong> reached out, asking for Will’s participation in <strong>Puppy Bowl 2019</strong>! Will, weighing a healthy 50 pounds, was too large to travel in the cabin of an airplane, so he and Bill set off on an exciting road trip to get him to New York, in time for the filming of Puppy Bowl 2019.</p>
+										<p class="mb-32">Because of the success of Bill’s injury-solution limb for Will, <a href="https://www.animalplanet.com/" target="_blank" title="animalplanet.com link"><strong>Animal Planet</strong></a> reached out, asking for Will’s participation in <a href="https://www.animalplanet.com/tv-shows/puppy-bowl/" target="_blank" title="Puppy Bowl 2019 website link"><strong>Puppy Bowl 2019</strong></a>! Will, weighing a healthy 50 pounds, was too large to travel in the cabin of an airplane, so he and Bill set off on an exciting road trip to get him to New York, in time for the filming of Puppy Bowl 2019.</p>
 									</div>
 								</div>
 								<div class="cell medium-shrink animal-planet-logo-img-cell">
 									<div class="img-wrap">
-										<img src="/img/partners/animal-planet-logo.png" alt="">
+										<a href="https://www.animalplanet.com/" target="_blank" title="animalplanet.com link">
+											<img src="/img/partners/animal-planet-logo.png" alt="Animal Planet logo">
+										</a>
 									</div>
 								</div>
 							</div>
