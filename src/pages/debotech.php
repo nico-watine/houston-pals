5ac979215ed93556e511a207a79d433ae1369a70
Nico Watine <nico@studioncreations.com>
2019-02-01 10:13:28 -0600

    Tweak width and border-width

--- src/pages/debotech.php
+++ src/pages/debotech.php
@@ -13,7 +13,7 @@ layout: default
 	<div class="grid-x grid-padding-x">
 		<div class="cell py-40">
 			<div class="thank-you-badge-wrap">
-				<div class="thank-you-badge" style="overflow: hidden; margin: 0 auto 24px; width: 280px; border-radius: 6px; border: 2px solid #30659B;">
+				<div class="thank-you-badge" style="overflow: hidden; margin: 0 auto 24px; max-width: 280px; border-radius: 6px; border: 3px solid #30659B;">
 					<span class="display-block text-center" style="background: #30659B;">
 						<p class="color-white py-12 m-0" style="line-height: 1; letter-spacing: 0.5px; text-transform: uppercase;"><strong>Supplier Thank You</strong></p>
 					</span>
