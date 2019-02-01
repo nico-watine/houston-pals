3310b2598333131cc58a5f3e86513e8e50eddad3
Nico Watine <nico@studioncreations.com>
2019-02-01 10:12:24 -0600

    @small done

--- src/pages/debotech.php
+++ src/pages/debotech.php
@@ -11,7 +11,17 @@ layout: default
 ---
 <div class="grid-container">
 	<div class="grid-x grid-padding-x">
-		<div class="cell">
+		<div class="cell py-40">
+			<div class="thank-you-badge-wrap">
+				<div class="thank-you-badge" style="overflow: hidden; margin: 0 auto 24px; width: 280px; border-radius: 6px; border: 2px solid #30659B;">
+					<span class="display-block text-center" style="background: #30659B;">
+						<p class="color-white py-12 m-0" style="line-height: 1; letter-spacing: 0.5px; text-transform: uppercase;"><strong>Supplier Thank You</strong></p>
+					</span>
+					<span class="display-block text-center" style="background: #F7F7F7;">
+						<p class="h3 py-12 m-0 body-font" style="color: #30659B; line-height: 1;"><strong>deBotech, Inc.</strong></p>
+					</span>
+				</div>
+			</div>
 			<div class="debotech-logo-img-wrap text-center mb-24">
 				<img src="/img/partners/debotech-logo.png" style="width: 400px;" alt="">
 			</div>
