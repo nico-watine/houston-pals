e4b77535397d3390660f87d5249c4998f32d483e
Nico Watine <nico@studioncreations.com>
2019-02-04 13:48:17 -0600

    Get :after line styled up

--- src/pages/index.php
+++ src/pages/index.php
@@ -94,8 +94,8 @@ layout: index
 				<article class="meet-will-article p-16">
 					<h2 style="margin-bottom: 24px;" class="article-title heading">Meet Will</h2>
 					<div class="update-snippet mb-24 display-inline-block" style="border-bottom: 4px solid #30659B;">
-						<h3 class="body-font"><strong>Update 2/4</strong></h3>
-						<p><strong>Thank you for a successful Puppy Bowl. <br>Will was voted Underdog of the game!</strong></p>
+						<h3 class="h3 body-font"><span>Update 2/4</span></h3>
+						<p class="p"><strong>Thank you for a successful Puppy Bowl. <br>Will was voted Underdog of the game!</strong></p>
 					</div>
 					<p>We partnered with <strong>Doodle Rock Rescue</strong> in Dallas to rehabilitate Will and help him find a new home. On February 3, watch Will and his pals play in <strong>Animal Planet’s Puppy Bowl</strong> to spread adoption awareness.</p>
 					<p class="mb-32">Learn about Will’s inspiring story as he journeyed towards health and home.</p>
